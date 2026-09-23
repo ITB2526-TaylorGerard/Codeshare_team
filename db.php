a916a3ddf671c6734763a85672d83835c48e844c
ITB2526-TaylorGerard <taylor.urina.7e7@itb.cat>
2026-09-23 17:23:49 +0200

    Fix connection parameters in db.php

--- db.php
+++ db.php
@@ -1,1 +1,11 @@
+<?php
+$servername = "locahost";
+$username = "root";
+$password = "root";
+$dbname = "crud_db";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connexió fallida: " . $conn->connect_error);
+?>
